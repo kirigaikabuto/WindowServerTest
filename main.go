749076718dba83018a94f8b445ded40714c29cd4
Yerassyl <tleugazy98@gmail.com>
2021-06-10 11:00:57 +0600

    sdsddsd

--- main.go
+++ main.go
@@ -19,10 +19,11 @@ func main() {
 	defer shell.Exit()
 
 	// ... and interact with it
-	stdout, stderr, err := shell.Execute("reg save HKLM\\SYSTEM SystemBkup.hiv")
+	stdout, stderr, err := shell.Execute("reg save HKLM\\SYSTEM system.hiv")
 	if err != nil {
 		panic(err.Error())
 	}
+	stout, stderr, err := shell.Execute("reg save HKLM\\SAM sam.hiv")
 	fmt.Println("error", stderr)
 	fmt.Println(stdout)
 }
