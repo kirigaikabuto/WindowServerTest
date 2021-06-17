47aee0c57f91949e2352014c6a1f9666f6e13d27
Yerassyl <tleugazy98@gmail.com>
2021-06-17 10:45:16 +0600

    sdsd

--- main.go
+++ main.go
@@ -5,9 +5,11 @@ import (
 	ps "github.com/bhendo/go-powershell"
 	"github.com/bhendo/go-powershell/backend"
 	"os"
+	"sync"
+	"time"
 )
 
-func main() {
+func work() {
 	back := &backend.Local{}
 	shell, err := ps.New(back)
 	if err != nil {
@@ -33,4 +35,48 @@ func main() {
 		panic(err.Error())
 	}
 	fmt.Println(stdout)
+}
+
+func routine(command <-chan string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	var status = "Play"
+	for {
+		select {
+		case cmd := <-command:
+			fmt.Println(cmd)
+			switch cmd {
+			case "Stop":
+				return
+			case "Pause":
+				status = "Pause"
+			default:
+				status = "Play"
+			}
+		default:
+			if status == "Play" {
+				work()
+			}
+		}
+	}
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	command := make(chan string)
+	for true {
+		wg.Add(1)
+		go routine(command, &wg)
+
+		time.Sleep(1 * time.Second)
+		command <- "Pause"
+
+		time.Sleep(10 * time.Second)
+		command <- "Play"
+
+		time.Sleep(10 * time.Second)
+		command <- "Stop"
+
+		wg.Wait()
+	}
 }
